1fdfbbb8d3c78503338e5c420127cc8bc924a05e
girijachachada <girijachachada2@gmail.com>
2024-07-03 16:55:13 +0530

    Phone Reviews added to Front End

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from bson import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://review123:review123@cluster0.73f4jpp.mongodb.net/movie_reviews"
+app.config["MONGO_URI_MOVIES"] = "mongodb+srv://review123:review123@cluster0.73f4jpp.mongodb.net/movie_reviews"
+app.config["MONGO_URI_PHONES"] = "mongodb+srv://review123:review123@cluster0.73f4jpp.mongodb.net/phone_reviews"
 app.secret_key = "supersecretkey"
 
-mongo = PyMongo(app)
+mongo_movies = PyMongo(app, uri=app.config["MONGO_URI_MOVIES"])
+mongo_phones = PyMongo(app, uri=app.config["MONGO_URI_PHONES"])
 
 @app.route('/')
 def index():
@@ -17,20 +19,20 @@ def index():
 def home():
     # Retrieve user's first name from session
     first_name = session.get('first_name', 'Guest')
-    movies = mongo.db.movies.find()
-    return render_template('home.html', first_name=first_name, movies=movies)
+    movies = mongo_movies.db.movies.find()
+    phones = mongo_phones.db.phones2.find()
+    return render_template('home.html', first_name=first_name, movies=movies,phones=phones)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        user = mongo.db.users.find_one({'email': email})
+        user = mongo_movies.db.users.find_one({'email': email})
 
         if user and check_password_hash(user['password'], password):
             flash('Login successful!', 'success')
             session['first_name'] = user['first_name']  # Store first name in session
-            print("First name stored in session:", session['first_name'])  # Debug statement
             return redirect(url_for('home'))
         else:
             flash('Invalid email or password', 'danger')
@@ -46,10 +48,10 @@ def signup():
         password = request.form['password']
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
 
-        if mongo.db.users.find_one({'email': email}):
+        if mongo_movies.db.users.find_one({'email': email}):
             flash('Email already exists!', 'danger')
         else:
-            mongo.db.users.insert_one({'first_name': first_name, 'last_name': last_name, 'email': email, 'password': hashed_password})
+            mongo_movies.db.users.insert_one({'first_name': first_name, 'last_name': last_name, 'email': email, 'password': hashed_password})
             flash('Signup successful!', 'success')
             session['first_name'] = first_name
             return redirect(url_for('home'))
@@ -63,17 +65,40 @@ def show_reviews(movie_id):
     object_id = ObjectId(movie_id)
     
     # Query MongoDB collection for movie with specified _id
-    movie = mongo.db.movies.find_one({"_id": object_id})
+    movie = mongo_movies.db.movies.find_one({"_id": object_id})
     if movie:
         movie_name = movie.get('movie_name')
         # Also fetch reviews from another MongoDB collection based on movie name
         other_reviews = []
-        other_movie = mongo.db.letterboxd_movies.find_one({"title": movie_name})
+        other_movie = mongo_movies.db.letterboxd_movies.find_one({"title": movie_name})
         if other_movie:
             other_reviews = other_movie.get('popular_reviews', [])
         reviews = movie.get('reviews', [])
     
     return render_template('reviews.html', reviews=reviews, other_reviews=other_reviews)
+
+@app.route('/phone_reviews/<phone_id>')
+def show_reviews_phone(phone_id):
+    # Retrieve reviews for the selected phone from MongoDB
+    reviews = ""
+    object_id = ObjectId(phone_id)
+    
+    # Query MongoDB collection for phone with specified _id
+    phone = mongo_phones.db.phones2.find_one({"_id": object_id})
+    if phone:
+        phone_name = phone.get('phone_name') + " "  # Add one extra space
+        other_reviews = ""
+        other_phone = mongo_phones.db.phones.find_one({"phone_name": phone_name})
+        if other_phone:
+            other_reviews = other_phone.get('review', "")
+        reviews = phone.get('review', "")
+    
+    # If other_reviews is empty, set it to "Review not available"
+    if not other_reviews:
+        other_reviews = "Review not available"
+    
+    return render_template('phone_reviews.html', reviews=reviews, other_reviews=other_reviews)
+
 
 
 @app.route('/about')
